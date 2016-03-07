79cec1fee075daa3280dac498b232e17d1028c81
kpironkova <krisi.pironkova@gmail.com>
2016-03-07 23:50:27 +0200

    #5 Cleaning and upgrading example view

--- application/views/pages/recipes/catalog.php
+++ application/views/pages/recipes/catalog.php
@@ -12,7 +12,7 @@
 
 		<div class="row">
 			<div class="col-md-6">
-				<h1 class="mb-none"><strong>Shop</strong></h1>
+				<h1 class="mb-none"><strong>Recipes Catalog</strong></h1>
 				<p>Showing 1–12 of 25 results.</p>
 			</div>
 		</div>
@@ -21,307 +21,87 @@
 
 			<ul class="products product-thumb-info-list" data-plugin-masonry>
 				<li class="col-md-3 col-sm-6 col-xs-12 product">
-					<a href="shop-product-sidebar.html">
-						<span class="onsale">Sale!</span>
+					<span class="product-thumb-info catalog-box">
+						<a href="#">
+							<span class="product-thumb-info-image">
+								<span class="product-thumb-info-act">
+									<span class="product-thumb-info-act-left"><em>View</em></span>
+								</span>
+								<img alt="" class="img-responsive catalog-image" src="<?php echo base_url()?>assets/img/burger.jpg" alt="">
+							</span>
+						</a>
+						<span class="product-thumb-info-content">
+							<a href="shop-product-sidebar.html">
+								<h4>Burgers</h4>
+								<span class="price">
+									<ins><span class="amount">Main dish</span></ins>
+								</span>
+							</a>
+						</span>
+					</span>
+				</li>
+				<li class="col-md-3 col-sm-6 col-xs-12 product">
+					<span class="product-thumb-info catalog-box">
+						<a href="#">
+							<span class="product-thumb-info-image">
+								<span class="product-thumb-info-act">
+									<span class="product-thumb-info-act-left"><em>View</em></span>
+								</span>
+								<img alt="" class="img-responsive catalog-image" src="<?php echo base_url()?>assets/img/tortilla.jpg" alt="">
+							</span>
+						</a>
+						<span class="product-thumb-info-content">
+							<a href="shop-product-sidebar.html">
+								<h4>Tortillas</h4>
+								<span class="price">
+									<ins><span class="amount">Main dish</span></ins>
+								</span>
+							</a>
+						</span>
+					</span>
+				</li>
+				<li class="col-md-3 col-sm-6 col-xs-12 product">
+					<span class="product-thumb-info catalog-box">
+						<a href="#">
+							<span class="product-thumb-info-image">
+								<span class="product-thumb-info-act">
+									<span class="product-thumb-info-act-left"><em>View</em></span>
+								</span>
+								<img alt="" class="img-responsive catalog-image" src="<?php echo base_url()?>assets/img/fish.jpg" alt="">
+							</span>
+						</a>
+						<span class="product-thumb-info-content">
+							<a href="shop-product-sidebar.html">
+								<h4>Fish</h4>
+								<span class="price">
+									<ins><span class="amount">Main dish</span></ins>
+								</span>
+							</a>
+						</span>
+					</span>
+				</li>
+				<li class="col-md-3 col-sm-6 col-xs-12 product">
+				<span class="product-thumb-info catalog-box">
+					<a href="#">
+						<span class="product-thumb-info-image">
+							<span class="product-thumb-info-act">
+								<span class="product-thumb-info-act-left"><em>View</em></span>
+							</span>
+							<img alt="" class="img-responsive catalog-image" src="<?php echo base_url()?>assets/img/suffle.jpg" alt="">
+						</span>
 					</a>
-					<span class="product-thumb-info">
-						<a href="shop-cart.html" class="add-to-cart-product">
-							<span><i class="fa fa-shopping-cart"></i> Add to Cart</span>
-						</a>
+					<span class="product-thumb-info-content">
 						<a href="shop-product-sidebar.html">
-							<span class="product-thumb-info-image">
-								<span class="product-thumb-info-act">
-									<span class="product-thumb-info-act-left"><em>View</em></span>
-									<span class="product-thumb-info-act-right"><em><i class="fa fa-plus"></i> Details</em></span>
-								</span>
-								<img alt="" class="img-responsive" src="img/products/product-1.jpg">
+							<h4>Suffle</h4>
+							<span class="price">
+								<ins><span class="amount">Desert</span></ins>
 							</span>
 						</a>
-						<span class="product-thumb-info-content">
-							<a href="shop-product-sidebar.html">
-								<h4>Photo Camera</h4>
-								<span class="price">
-									<del><span class="amount">$325</span></del>
-									<ins><span class="amount">$299</span></ins>
-								</span>
-							</a>
-						</span>
 					</span>
-				</li>
-				<li class="col-md-3 col-sm-6 col-xs-12 product">
-					<span class="product-thumb-info">
-						<a href="shop-cart.html" class="add-to-cart-product">
-							<span><i class="fa fa-shopping-cart"></i> Add to Cart</span>
-						</a>
-						<a href="shop-product-sidebar.html">
-							<span class="product-thumb-info-image">
-								<span class="product-thumb-info-act">
-									<span class="product-thumb-info-act-left"><em>View</em></span>
-									<span class="product-thumb-info-act-right"><em><i class="fa fa-plus"></i> Details</em></span>
-								</span>
-								<img alt="" class="img-responsive" src="img/products/product-2.jpg">
-							</span>
-						</a>
-						<span class="product-thumb-info-content">
-							<a href="shop-product-sidebar.html">
-								<h4>Golf Bag</h4>
-								<span class="price">
-									<span class="amount">$72</span>
-								</span>
-							</a>
-						</span>
-					</span>
-				</li>
-				<li class="col-md-3 col-sm-6 col-xs-12 product">
-					<span class="product-thumb-info">
-						<a href="shop-cart.html" class="add-to-cart-product">
-							<span><i class="fa fa-shopping-cart"></i> Add to Cart</span>
-						</a>
-						<a href="shop-product-sidebar.html">
-							<span class="product-thumb-info-image">
-								<span class="product-thumb-info-act">
-									<span class="product-thumb-info-act-left"><em>View</em></span>
-									<span class="product-thumb-info-act-right"><em><i class="fa fa-plus"></i> Details</em></span>
-								</span>
-								<img alt="" class="img-responsive" src="img/products/product-3.jpg">
-							</span>
-						</a>
-						<span class="product-thumb-info-content">
-							<a href="shop-product-sidebar.html">
-								<h4>Workout</h4>
-								<span class="price">
-									<span class="amount">$60</span>
-								</span>
-							</a>
-						</span>
-					</span>
-				</li>
-				<li class="col-md-3 col-sm-6 col-xs-12 product">
-					<span class="product-thumb-info">
-						<a href="shop-cart.html" class="add-to-cart-product">
-							<span><i class="fa fa-shopping-cart"></i> Add to Cart</span>
-						</a>
-						<a href="shop-product-sidebar.html">
-							<span class="product-thumb-info-image">
-								<span class="product-thumb-info-act">
-									<span class="product-thumb-info-act-left"><em>View</em></span>
-									<span class="product-thumb-info-act-right"><em><i class="fa fa-plus"></i> Details</em></span>
-								</span>
-								<img alt="" class="img-responsive" src="img/products/product-4.jpg">
-							</span>
-						</a>
-						<span class="product-thumb-info-content">
-							<a href="shop-product-sidebar.html">
-								<h4>Luxury bag</h4>
-								<span class="price">
-									<span class="amount">$199</span>
-								</span>
-							</a>
-						</span>
-					</span>
-				</li>
-				<li class="col-md-3 col-sm-6 col-xs-12 product">
-					<span class="product-thumb-info">
-						<a href="shop-cart.html" class="add-to-cart-product">
-							<span><i class="fa fa-shopping-cart"></i> Add to Cart</span>
-						</a>
-						<a href="shop-product-sidebar.html">
-							<span class="product-thumb-info-image">
-								<span class="product-thumb-info-act">
-									<span class="product-thumb-info-act-left"><em>View</em></span>
-									<span class="product-thumb-info-act-right"><em><i class="fa fa-plus"></i> Details</em></span>
-								</span>
-								<img alt="" class="img-responsive" src="img/products/product-5.jpg">
-							</span>
-						</a>
-						<span class="product-thumb-info-content">
-							<a href="shop-product-sidebar.html">
-								<h4>Ladies' handbag</h4>
-								<span class="price">
-									<span class="amount">$189</span>
-								</span>
-							</a>
-						</span>
-					</span>
-				</li>
-				<li class="col-md-3 col-sm-6 col-xs-12 product">
-					<a href="shop-product-sidebar.html">
-						<span class="onsale">Sale!</span>
-					</a>
-					<span class="product-thumb-info">
-						<a href="shop-cart.html" class="add-to-cart-product">
-							<span><i class="fa fa-shopping-cart"></i> Add to Cart</span>
-						</a>
-						<a href="shop-product-sidebar.html">
-							<span class="product-thumb-info-image">
-								<span class="product-thumb-info-act">
-									<span class="product-thumb-info-act-left"><em>View</em></span>
-									<span class="product-thumb-info-act-right"><em><i class="fa fa-plus"></i> Details</em></span>
-								</span>
-								<img alt="" class="img-responsive" src="img/products/product-6.jpg">
-							</span>
-						</a>
-						<span class="product-thumb-info-content">
-							<a href="shop-product-sidebar.html">
-								<h4>Baseball Cap</h4>
-								<span class="price">
-									<del><span class="amount">$25</span></del>
-									<ins><span class="amount">$22</span></ins>
-								</span>
-							</a>
-						</span>
-					</span>
-				</li>
-				<li class="col-md-3 col-sm-6 col-xs-12 product">
-					<span class="product-thumb-info">
-						<a href="shop-cart.html" class="add-to-cart-product">
-							<span><i class="fa fa-shopping-cart"></i> Add to Cart</span>
-						</a>
-						<a href="shop-product-sidebar.html">
-							<span class="product-thumb-info-image">
-								<span class="product-thumb-info-act">
-									<span class="product-thumb-info-act-left"><em>View</em></span>
-									<span class="product-thumb-info-act-right"><em><i class="fa fa-plus"></i> Details</em></span>
-								</span>
-								<img alt="" class="img-responsive" src="img/products/product-7.jpg">
-							</span>
-						</a>
-						<span class="product-thumb-info-content">
-							<a href="shop-product-sidebar.html">
-								<h4>Blue Ladies Handbag</h4>
-								<span class="price">
-									<span class="amount">$290</span>
-								</span>
-							</a>
-						</span>
-					</span>
-				</li>
-				<li class="col-md-3 col-sm-6 col-xs-12 product">
-					<span class="product-thumb-info">
-						<a href="shop-cart.html" class="add-to-cart-product">
-							<span><i class="fa fa-shopping-cart"></i> Add to Cart</span>
-						</a>
-						<a href="shop-product-sidebar.html">
-							<span class="product-thumb-info-image">
-								<span class="product-thumb-info-act">
-									<span class="product-thumb-info-act-left"><em>View</em></span>
-									<span class="product-thumb-info-act-right"><em><i class="fa fa-plus"></i> Details</em></span>
-								</span>
-								<img alt="" class="img-responsive" src="img/products/product-8.jpg">
-							</span>
-						</a>
-						<span class="product-thumb-info-content">
-							<a href="shop-product-sidebar.html">
-								<h4>Military Rucksack</h4>
-								<span class="price">
-									<span class="amount">$49</span>
-								</span>
-							</a>
-						</span>
-					</span>
-				</li>
-				<li class="col-md-3 col-sm-6 col-xs-12 product">
-					<a href="shop-product-sidebar.html">
-						<span class="onsale">Sale!</span>
-					</a>
-					<span class="product-thumb-info">
-						<a href="shop-cart.html" class="add-to-cart-product">
-							<span><i class="fa fa-shopping-cart"></i> Add to Cart</span>
-						</a>
-						<a href="shop-product-sidebar.html">
-							<span class="product-thumb-info-image">
-								<span class="product-thumb-info-act">
-									<span class="product-thumb-info-act-left"><em>View</em></span>
-									<span class="product-thumb-info-act-right"><em><i class="fa fa-plus"></i> Details</em></span>
-								</span>
-								<img alt="" class="img-responsive" src="img/products/product-9.jpg">
-							</span>
-						</a>
-						<span class="product-thumb-info-content">
-							<a href="shop-product-sidebar.html">
-								<h4>Baseball</h4>
-								<span class="price">
-									<del><span class="amount">$15</span></del>
-									<ins><span class="amount">$12</span></ins>
-								</span>
-							</a>
-						</span>
-					</span>
-				</li>
-				<li class="col-md-3 col-sm-6 col-xs-12 product">
-					<span class="product-thumb-info">
-						<a href="shop-cart.html" class="add-to-cart-product">
-							<span><i class="fa fa-shopping-cart"></i> Add to Cart</span>
-						</a>
-						<a href="shop-product-sidebar.html">
-							<span class="product-thumb-info-image">
-								<span class="product-thumb-info-act">
-									<span class="product-thumb-info-act-left"><em>View</em></span>
-									<span class="product-thumb-info-act-right"><em><i class="fa fa-plus"></i> Details</em></span>
-								</span>
-								<img alt="" class="img-responsive" src="img/products/product-10.jpg">
-							</span>
-						</a>
-						<span class="product-thumb-info-content">
-							<a href="shop-product-sidebar.html">
-								<h4>Travel bag</h4>
-								<span class="price">
-									<span class="amount">$78</span>
-								</span>
-							</a>
-						</span>
-					</span>
-				</li>
-				<li class="col-md-3 col-sm-6 col-xs-12 product">
-					<span class="product-thumb-info">
-						<a href="shop-cart.html" class="add-to-cart-product">
-							<span><i class="fa fa-shopping-cart"></i> Add to Cart</span>
-						</a>
-						<a href="shop-product-sidebar.html">
-							<span class="product-thumb-info-image">
-								<span class="product-thumb-info-act">
-									<span class="product-thumb-info-act-left"><em>View</em></span>
-									<span class="product-thumb-info-act-right"><em><i class="fa fa-plus"></i> Details</em></span>
-								</span>
-								<img alt="" class="img-responsive" src="img/products/product-11.jpg">
-							</span>
-						</a>
-						<span class="product-thumb-info-content">
-							<a href="shop-product-sidebar.html">
-								<h4>Pair of Ice Skates</h4>
-								<span class="price">
-									<span class="amount">$220</span>
-								</span>
-							</a>
-						</span>
-					</span>
-				</li>
-				<li class="col-md-3 col-sm-6 col-xs-12 product">
-					<span class="product-thumb-info">
-						<a href="shop-cart.html" class="add-to-cart-product">
-							<span><i class="fa fa-shopping-cart"></i> Add to Cart</span>
-						</a>
-						<a href="shop-product-sidebar.html">
-							<span class="product-thumb-info-image">
-								<span class="product-thumb-info-act">
-									<span class="product-thumb-info-act-left"><em>View</em></span>
-									<span class="product-thumb-info-act-right"><em><i class="fa fa-plus"></i> Details</em></span>
-								</span>
-								<img alt="" class="img-responsive" src="img/products/product-12.jpg">
-							</span>
-						</a>
-						<span class="product-thumb-info-content">
-							<a href="shop-product-sidebar.html">
-								<h4>Sun Glasses</h4>
-								<span class="price">
-									<span class="amount">$129</span>
-								</span>
-							</a>
-						</span>
-					</span>
-				</li>
+				</span>
+			</li>
+				
 			</ul>
-
 		</div>
 
 		<div class="row">
