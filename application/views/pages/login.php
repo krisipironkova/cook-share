5ddf6a02d435bc5cf41ef4de66283f46b27fa81a
kpironkova <krisi.pironkova@gmail.com>
2016-03-16 22:29:06 +0200

    #12 Login form view

--- application/views/pages/login.php
+++ application/views/pages/login.php
@@ -1,1 +1,42 @@
-<?php echo 'login form'; ?>
+<div role="main" class="main">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="featured-boxes">
+                <div class="row">
+                  <div class="col-sm-6 col-md-offset-3">
+                    <div class="featured-box featured-box-primary align-left mt-xlg">
+                      <div class="box-content login-box">
+                        <h4 class="heading-primary text-uppercase mb-md">Login</h4>
+                          <div class="row">
+                            <div class="form-group">
+                              <div class="col-md-12">
+                                <label>E-mail Address</label>
+                                <input type="text" name="email" value="" class="form-control input-lg" />
+                              </div>
+                            </div>
+                          </div>
+                          <div class="row">
+                            <div class="form-group">
+                              <div class="col-md-12">
+                                <a class="pull-right" href="#">(Forgot Password?)</a>
+                                <label>Password</label>
+                                <input type="password" name="password" class="form-control input-lg">
+                              </div>
+                            </div>
+                          </div>
+                          <div class="row">
+                            <div class="col-md-12">
+                              <input type="submit" value="Login" class="btn btn-primary pull-right mb-xl" data-loading-text="Loading...">
+                            </div>
+                          </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
