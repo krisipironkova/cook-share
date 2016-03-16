f517774373491ea4e4a25dc0399b82906ea1441d
kpironkova <krisi.pironkova@gmail.com>
2016-03-16 22:47:51 +0200

    #13 Register method - loadin register test view

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -3,7 +3,11 @@
 class Users extends CI_Controller{
 
 	public function login(){
-		$this->layout->render('login');
+		$this->layout->render('users/login');
+	}
+
+	public function register(){
+		$this->layout->render('users/register');
 	}
 }
 
