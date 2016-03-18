e58ffcf9c1eb7d210b7292a006cd4dceed362eb3
kpironkova <krisi.pironkova@gmail.com>
2016-03-18 19:07:37 +0200

    #15 REST Skelethon - Api controller

---
+++ application/controllers/Api.php
@@ -0,0 +1,23 @@
+
+<?php
+
+header('Access-Control-Allow-Origin: *');
+
+require APPPATH . '/libraries/REST_Controller.php';
+
+class Api extends REST_Controller {
+
+    public function users_get() {
+
+    	
+    }
+
+    public function users_post() {
+       
+    }
+
+    public function users_put() {
+       
+    }
+
+}
