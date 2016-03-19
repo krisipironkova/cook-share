a8565593811b2c95e0d39b721c230f0dafce34bd
kpironkova <krisi.pironkova@gmail.com>
2016-03-19 23:10:56 +0200

    #18 Api start up - users_get and users_post methods - in progress

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -9,15 +9,27 @@ class Api extends REST_Controller {
 
     public function users_get() {
 
-    	
+        if (empty($id = $this->get('id'))) {
+            $users = $this->user_model->get_users();
+	        $this->response($users, REST_Controller::HTTP_OK);
+
+        } else {
+        	if ($user = $this->user_model->get_user($this->get('id'))) {
+        		$this->response($user, REST_Controller::HTTP_OK);
+        	} else {
+                $this->response([
+                    'status' => FALSE,
+                    'message' => 'No user found'
+                ], REST_Controller::HTTP_NOT_FOUND);
+        	}
+        }
     }
 
-    public function users_post() {
-       
-    }
+    public function users_post() {    
+        // ToDo: check if user already exists
 
-    public function users_put() {
-       
+        $this->user_model->set_user($this->post());
+        $this->response([ 'status' => TRUE, 'message' => 'User created successfully!'], REST_Controller::HTTP_OK);
     }
 
 }
