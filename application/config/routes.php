0bd898e42ef6157bcb0ac1e2bcdfc2d1a7bdeb34
kpironkova <krisi.pironkova@gmail.com>
2016-03-19 23:22:33 +0200

    #21 Routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'pages/home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+$route['users/(:num)'] = 'api/users/id/$1';
+$route['users'] = 'api/users';
