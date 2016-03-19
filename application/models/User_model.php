00b728b4b66535e21e2599a7a3740bd78c0b1b4a
kpironkova <krisi.pironkova@gmail.com>
2016-03-19 22:36:32 +0200

    #17 User_model.php

---
+++ application/models/User_model.php
@@ -0,0 +1,26 @@
+<?php
+
+class User_model extends CI_Model {
+	private $table = 'users';
+	private $columns = 'id, username, email';
+
+	public function set_user($info){
+
+		unset($info['passconf']);
+		$info['password'] = sha1($info['password']);
+
+		$this->db->insert($this->table, $info);
+	}
+
+	public function get_user($id) {
+		return $this->db->select($this->columns)->where('id', $id)->get($this->table)->result();
+	}
+
+	public function get_users() {
+		return $this->db->select($this->columns)->get($this->table)->result();
+	}
+
+	public function update_user($id, $info) {
+
+	}
+}
