ad5d15d06d1189f5de845fe58844d9875168db2c
kpironkova <krisi.pironkova@gmail.com>
2016-03-30 22:59:44 +0300

    #26 Common hook configuration

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -11,3 +11,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |	https://codeigniter.com/user_guide/general/hooks.html
 |
 */
+$hook['post_controller_constructor'][] = array(
+    'class' => 'Common',
+    'function' => 'user_info',
+    'filename' => 'Common.php',
+    'filepath' => 'hooks',
+    'params' => array() );
