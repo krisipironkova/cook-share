cfb6d76419e60cf98f9e1bd6545dbd28717e6592
kpironkova <krisi.pironkova@gmail.com>
2016-04-11 19:28:54 +0300

    #36 Removing Catalog controller and moving recipe_view in Recipes controller

--- application/controllers/Catalog.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Catalog extends CI_Controller{
-
-	public function recipe_view(){
-		$this->layout->render('recipes/recipe_view');
-	}
-}
--- application/controllers/Recipes.php
+++ application/controllers/Recipes.php
@@ -2,8 +2,9 @@
 
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Recipes extends CI_Controller
-{
+class Recipes extends CI_Controller{
+
+	public $data;
 
     public function index(){
 
@@ -11,10 +12,16 @@ class Recipes extends CI_Controller
     }
 
     public function my_recipes(){
+
+    	$this->data['recipes'] = $this->recipe_model->get_all();
     	$this->layout->render('recipes/my_recipes');
     }
 
     public function recipe_form(){
     	$this->layout->render('recipes/recipe_form');
     }
+
+    public function recipe_view(){
+		$this->layout->render('recipes/recipe_view');
+	}
 }
