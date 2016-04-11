b9d455dcfb5e7cb462ee3773e6d5fcfee4dff0f7
kpironkova <krisi.pironkova@gmail.com>
2016-04-11 20:42:12 +0300

    #38 Recipes catalog listing with id

--- application/views/pages/recipes/catalog.php
+++ application/views/pages/recipes/catalog.php
@@ -11,106 +11,28 @@
 		<div class="row">
 			<div class="col-md-6">
 				<h1 class="mb-none"><strong>Recipes Catalog</strong></h1>
-				<p>Showing 1–12 of 25 results.</p>
 			</div>
 		</div>
 
 		<div class="row">
-
-			<ul class="products product-thumb-info-list" data-plugin-masonry>
-				<li class="col-md-3 col-sm-6 col-xs-12 product">
-					<span class="product-thumb-info catalog-box">
-						<a href="<?=base_url('catalog/recipe_view')?>">
-							<span class="product-thumb-info-image">
-								<span class="product-thumb-info-act">
-									<span class="product-thumb-info-act-left"><em>View</em></span>
-								</span>
-								<img alt="" class="img-responsive catalog-image" src="<?php echo base_url()?>assets/img/burger.jpg" alt="">
-							</span>
-						</a>
-						<span class="product-thumb-info-content">
-							<a href="shop-product-sidebar.html">
-								<h4>Burgers</h4>
-								<span class="price">
-									<ins><span class="amount">Main dish</span></ins>
+				<ul class="products product-thumb-info-list" data-plugin-masonry>
+				<?php foreach($recipes as $recipe) : ?>
+					<li class="col-md-3 col-sm-6 col-xs-12 product">
+						<span class="product-thumb-info catalog-box">
+							<a href="<?=base_url('recipes/recipe_view/'.$recipe->id)?>">
+								<span class="product-thumb-info-image">
+									<span class="product-thumb-info-act">
+										<span class="product-thumb-info-act-left"><em>View</em></span>
+									</span>
+									<img alt="" class="img-responsive catalog-image" src="<?php echo base_url()?>assets/img/burger.jpg" alt="">
 								</span>
 							</a>
-						</span>
-					</span>
-				</li>
-				<li class="col-md-3 col-sm-6 col-xs-12 product">
-					<span class="product-thumb-info catalog-box">
-						<a href="#">
-							<span class="product-thumb-info-image">
-								<span class="product-thumb-info-act">
-									<span class="product-thumb-info-act-left"><em>View</em></span>
-								</span>
-								<img alt="" class="img-responsive catalog-image" src="<?php echo base_url()?>assets/img/tortilla.jpg" alt="">
+							<span class="product-thumb-info-content">
+									<h4><?php echo $recipe->title ?></h4>			
 							</span>
-						</a>
-						<span class="product-thumb-info-content">
-							<a href="shop-product-sidebar.html">
-								<h4>Tortillas</h4>
-								<span class="price">
-									<ins><span class="amount">Main dish</span></ins>
-								</span>
-							</a>
 						</span>
-					</span>
-				</li>
-				<li class="col-md-3 col-sm-6 col-xs-12 product">
-					<span class="product-thumb-info catalog-box">
-						<a href="#">
-							<span class="product-thumb-info-image">
-								<span class="product-thumb-info-act">
-									<span class="product-thumb-info-act-left"><em>View</em></span>
-								</span>
-								<img alt="" class="img-responsive catalog-image" src="<?php echo base_url()?>assets/img/fish.jpg" alt="">
-							</span>
-						</a>
-						<span class="product-thumb-info-content">
-							<a href="shop-product-sidebar.html">
-								<h4>Fish</h4>
-								<span class="price">
-									<ins><span class="amount">Main dish</span></ins>
-								</span>
-							</a>
-						</span>
-					</span>
-				</li>
-				<li class="col-md-3 col-sm-6 col-xs-12 product">
-				<span class="product-thumb-info catalog-box">
-					<a href="#">
-						<span class="product-thumb-info-image">
-							<span class="product-thumb-info-act">
-								<span class="product-thumb-info-act-left"><em>View</em></span>
-							</span>
-							<img alt="" class="img-responsive catalog-image" src="<?php echo base_url()?>assets/img/suffle.jpg" alt="">
-						</span>
-					</a>
-					<span class="product-thumb-info-content">
-						<a href="shop-product-sidebar.html">
-							<h4>Suffle</h4>
-							<span class="price">
-								<ins><span class="amount">Desert</span></ins>
-							</span>
-						</a>
-					</span>
-				</span>
-			</li>
-				
-			</ul>
-		</div>
-
-		<div class="row">
-			<div class="col-md-12">
-				<ul class="pagination pull-right">
-					<li><a href="#"><i class="fa fa-chevron-left"></i></a></li>
-					<li class="active"><a href="#">1</a></li>
-					<li><a href="#">2</a></li>
-					<li><a href="#">3</a></li>
-					<li><a href="#"><i class="fa fa-chevron-right"></i></a></li>
+					</li>	
+					<?php endforeach; ?>
 				</ul>
-			</div>
 		</div>
 </div>
