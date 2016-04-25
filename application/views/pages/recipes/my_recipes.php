13e88d875eeba53143f8dc823dde43978acb588f
kpironkova <krisi.pironkova@gmail.com>
2016-04-25 19:54:55 +0300

    #45 Customizing my_recipes and favourites views

--- application/views/pages/recipes/my_recipes.php
+++ application/views/pages/recipes/my_recipes.php
@@ -25,11 +25,11 @@
 		<div class="row">
 			<ul class="portfolio-list">
 			<?php foreach ($my_recipes as $recipe): ?>
-				<li class="col-md-2 col-sm-3 col-xs-3 isotope-item websites">
+				<li class="col-md-2 col-sm-3 col-xs-3 my_recipes">
 					<div class="portfolio-item">
 						<a href="<?=base_url('recipes/recipe_view/'.$recipe->id)?>">
 							<span class="thumb-info">
-								<span class="thumb-info-wrapper">
+								<span class="thumb-info-wrapper my_recipes_image">
 									<img src="<?php echo base_url() . 'assets/uploads/' . $recipe->photo; ?>" class="img-responsive" alt="">
 									<span class="thumb-info-title">
 										<span class="thumb-info-inner"><?php echo $recipe->title ?></span>
